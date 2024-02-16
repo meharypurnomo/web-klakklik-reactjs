3399fab875e5811425565b2662d00883aae46108
alvittoag <alvittogenz09@gmail.com>
2024-02-16 15:40:53 +0700

    fix : padding card side

--- src/elements/CardSide.tsx
+++ src/elements/CardSide.tsx
@@ -6,7 +6,7 @@ const CardSide = ({
   title: string;
 }) => {
   return (
-    <div className="border space-y-10 rounded-3xl pt-7 pb-10 px-9">
+    <div className="border space-y-10 rounded-3xl pt-7 pb-10 px-5 md:px-9">
       <div className="space-y-3">
         <h1 className="text-xl font-semibold text-slate-800">{title}</h1>
 
